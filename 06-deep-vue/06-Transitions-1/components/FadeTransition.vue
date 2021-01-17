210eb60bffcc26e2c77e58a06c4f951cb78801fa
Oleh Parshyn <ooparshyn@gmail.com>
2021-01-17 20:56:39 +0200

    [6-5]

--- 06-deep-vue/06-Transitions-1/components/FadeTransition.vue
+++ 06-deep-vue/06-Transitions-1/components/FadeTransition.vue
@@ -1,12 +1,20 @@
-<template></template>
+<template>
+  <transition name="fade" mode="out-in">
+    <slot v-on="$listeners" v-bind="$attrs" />
+  </transition>
+</template>
 
 <script>
 export default {
   name: 'FadeTransition',
+  inheritAttrs: false,
 };
 </script>
 
 <style scoped>
+.fade-enter-active {
+  animation: fade 0.2s ease-in;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.2s ease-in-out;
