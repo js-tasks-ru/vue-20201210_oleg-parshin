de30ae0f803a04019b6b9a6b9e3f718b205c11a9
Oleh Parshyn <ooparshyn@gmail.com>
2021-01-18 10:44:04 +0200

    [6-7]

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,22 @@
 <script>
 export default {
   name: 'NodesPagination',
+  props: {
+    page: {
+      type: Number,
+      default: 0,
+    },
+    perPage: {
+      type: Number,
+      default: 5,
+    },
+  },
+  render(h) {
+    const startIdx = (this.page - 1) * this.perPage;
+    const endIdx = startIdx + this.perPage;
+    const elemsOnPage = (this.$slots.default || []).slice(startIdx, endIdx);
+    return h('div', elemsOnPage);
+  },
 };
 </script>
 
