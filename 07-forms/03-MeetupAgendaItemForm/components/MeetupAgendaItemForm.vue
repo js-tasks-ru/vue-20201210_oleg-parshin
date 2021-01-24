d70c481183ff4bee13b5cc5bdb58ee5bc0abf1f4
Oleh Parshyn <ooparshyn@gmail.com>
2021-01-24 13:08:36 +0200

    [7-3]

--- 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="form-section form-section_inner">
-    <button type="button" class="remove-button">
+    <button type="button" class="remove-button" @click="$emit('remove')">
       <img src="../assets/icons/icon-trash.svg" alt="trash" />
     </button>
 
     <div class="form-group">
-      <select class="form-control" title="Тип">
-        <option value="talk">Доклад</option>
-        <option value="other">Другое</option>
+      <select class="form-control" title="Тип" v-model="agendaItem_.type">
+        <option disabled value="">Выберете тип</option>
+        <option v-for="type in types" :key="type.text" :value="type.value">
+          {{ type.text }}
+        </option>
       </select>
     </div>
 
@@ -15,28 +17,53 @@
       <div class="form__col">
         <div class="form-group">
           <label class="form-label">Начало</label>
-          <input class="form-control" type="time" placeholder="00:00" />
+          <input
+            class="form-control"
+            type="time"
+            placeholder="00:00"
+            :value="agendaItem_.startsAt"
+            @input="onInput($event, 'startsAt')"
+          />
         </div>
       </div>
       <div class="form__col">
         <div class="form-group">
           <label class="form-label">Окончание</label>
-          <input class="form-control" type="time" placeholder="00:00" />
+          <input
+            class="form-control"
+            type="time"
+            placeholder="00:00"
+            v-model="agendaItem_.endsAt"
+          />
         </div>
       </div>
     </div>
 
     <div class="form-group">
-      <label class="form-label">Заголовок</label>
-      <input class="form-control" />
+      <label class="form-label">{{ title }}</label>
+      <input class="form-control" v-model="agendaItem_.title" />
     </div>
-    <div class="form-group">
+
+    <div v-if="isTalk" class="form-group">
+      <label class="form-label">Докладчик</label>
+      <input class="form-control" v-model="agendaItem_.speaker" />
+    </div>
+
+    <div v-if="isTalk || isOther" class="form-group">
       <label class="form-label">Описание</label>
-      <textarea class="form-control"></textarea>
+      <textarea
+        class="form-control"
+        v-model="agendaItem_.description"
+      ></textarea>
     </div>
-    <div class="form-group">
+
+    <div v-if="isTalk" class="form-group">
       <label class="form-label">Язык</label>
-      <select class="form-control"></select>
+      <select class="form-control" title="Язык" v-model="agendaItem_.language">
+        <option v-for="lang in langs" :key="lang.text" :value="lang.value">
+          {{ lang.text }}
+        </option>
+      </select>
     </div>
   </div>
 </template>
@@ -61,6 +88,86 @@ const getTalkLanguages = () => [
 
 export default {
   name: 'MeetupAgendaItemForm',
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      agendaItem_: { ...this.agendaItem },
+    };
+  },
+  computed: {
+    types() {
+      return getAgendaItemTypes();
+    },
+    langs() {
+      return getTalkLanguages();
+    },
+    isTalk() {
+      return this.agendaItem_.type === 'talk';
+    },
+    isOther() {
+      return this.agendaItem_.type === 'other';
+    },
+    title() {
+      let title = 'Нестандартный текст (необязательно)';
+      switch (this.agendaItem_.type) {
+        case 'talk':
+          title = 'Тема';
+          break;
+        case 'other':
+          title = 'Заголовок';
+          break;
+      }
+      return title;
+    },
+  },
+  watch: {
+    agendaItem_: {
+      deep: true,
+      handler(newVal, oldVal) {
+        if (newVal !== oldVal) {
+          this.syncTime(newVal, oldVal);
+        }
+        this.$emit('update:agendaItem', { ...newVal });
+      },
+    },
+  },
+  methods: {
+    onInput(e, field) {
+      this.agendaItem_ = {
+        ...this.agendaItem_,
+        [field]: e.target.value,
+      };
+    },
+    format(n) {
+      return n.length === 1 ? 0 + n : n;
+    },
+    matchStr(str) {
+      return str.match(/\d{2}/g);
+    },
+    syncTime(newVal, oldVal) {
+      let hours = 0;
+      let [startsNewH] = this.matchStr(newVal.startsAt);
+      let [startsOldH] = this.matchStr(oldVal.startsAt);
+      let [endsH] = this.matchStr(this.agendaItem_.endsAt);
+      startsNewH = +startsNewH;
+      startsOldH = +startsOldH;
+      endsH = +endsH;
+      let dif = 0;
+      if (startsNewH > startsOldH) {
+        dif = startsNewH - startsOldH;
+      } else {
+        dif = 24 + startsNewH - startsOldH;
+      }
+      hours = (endsH + dif) % 24;
+      hours = this.format(hours.toString());
+      newVal.endsAt = `${hours}:00`;
+    },
+  },
 };
 </script>
 
